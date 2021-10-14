bfbd83ca2805eb168bb2c647a606a591eb1c7240
Jani Palomino <palomino.jani@gmail.com>
2021-10-14 16:51:51 -0600

    Update to include methods

--- src/Round.js
+++ src/Round.js
@@ -1,11 +1,36 @@
-class Deck {
-  constructor(cards) {
-    this.cards = cards;
+const Turn = require('../src/Turn');
+
+class Round {
+  constructor(deck) {
+    this.deck = deck.cards;
+    this.turns = 0;
+    this.incorrectGuesses = [];
   }
 
-  countCards() {
-    return this.cards.length;
+  returnCurrentCard() {
+    return this.deck[this.turns];
+  }
+
+  takeTurn(guess) {
+    const currentCard = this.returnCurrentCard();
+    const turn = new Turn(guess, currentCard);
+    this.turns++;
+
+    if (!turn.evaluateGuess()) {
+      this.incorrectGuesses.push(currentCard.id);
+    }
+    return turn.giveFeedback();
+  }
+
+  calculatePercentCorrect() {
+    const percent = Math.floor(((this.turns - this.incorrectGuesses.length) / this.turns) * 100);
+    return percent;
+  }
+
+  endRound() {
+    const percentCorrect = this.calculatePercentCorrect();
+    return `** Round over! ** You answered ${percentCorrect}% of the questions correctly!`;
   }
 };
 
-module.exports = Deck;
+module.exports = Round;
