a5b9fed7a2d4e30d8ead7197b721fa7a26773848
Jani Palomino <palomino.jani@gmail.com>
2021-10-14 16:15:50 -0600

    Remove skips

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,17 +6,17 @@ const Card = require('../src/Card');
 
 describe('Deck', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const deck = new Deck();
     expect(Deck).to.be.a('function');
   });
 
-  it.skip('should be an instance of Deck', function() {
+  it('should be an instance of Deck', function() {
     const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
 
-  it.skip('should contain several cards', function() {
+  it('should contain several cards', function() {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
     const card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
@@ -25,7 +25,7 @@ describe('Deck', function() {
     expect(deck.cards).to.be.a('array');
   });
 
-  it.skip('should contain Card objects', function() {
+  it('should contain Card objects', function() {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
     const card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
@@ -34,7 +34,7 @@ describe('Deck', function() {
     expect(deck.cards[0]).to.be.an.instanceof(Card);
   });
 
-  it.skip('should know how many Cards are in the deck', function() {
+  it('should know how many Cards are in the deck', function() {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
     const card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
