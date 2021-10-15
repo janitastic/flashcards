3e3b34f6b3fbe90ad5ee44242a5bbb722cc63e1c
Jani Palomino <palomino.jani@gmail.com>
2021-10-14 19:55:39 -0600

    Update start method

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,33 @@
 const data = require('./data');
-const prototypeQuestions = data.prototypeData;//will need to figure out what this is used for
+const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('./Card');
+const Deck = require('./Deck');
+const Round = require('./Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = 0;
+  }
 
-  printMessage(deck, round)//will need to pull in round {
+  //note lint indicates round is never used below
+  printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
 
   printQuestion(round) {
     util.main(round);
+  }
+
+  start() {
+    const cards = prototypeQuestions.map((card) => {
+      return card = new Card(card.id, card.question, card.answers, card.correctAnswer)
+    });
+    const deck = new Deck(cards);
+      this.currentRound = new Round(deck);
+      this.printMessage(deck, this.currentRound);
+      this.printQuestion(this.currentRound);
   }
 }
 
