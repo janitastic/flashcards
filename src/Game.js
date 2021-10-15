34b4ea245d6412e3dd300a1de3b5b8f014a504ac
Jani Palomino <palomino.jani@gmail.com>
2021-10-14 18:03:37 -0600

    Refactor after running lint

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,18 @@
 const data = require('./data');
-const prototypeQuestions = data.prototypeData;
+const prototypeQuestions = data.prototypeData;//will need to figure out what this is used for
 const util = require('./util');
 
 class Game {
   constructor() {}
 
-  printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+  printMessage(deck, round)//will need to pull in round {
+    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
 
   printQuestion(round) {
-      util.main(round);
+    util.main(round);
   }
-};
+}
 
 module.exports = Game;
